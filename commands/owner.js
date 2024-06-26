5e0814cec4443e555ec2c68813a5abe2d78bdfcf
Zerokaloria5 <165279308+Zerokaloria5@users.noreply.github.com>
2024-06-26 15:02:17 +0200

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -29,17 +29,17 @@ module.exports = {
 
   run: async (client, interaction) => {
     try {
-      const youtubeLink = 'https://discord.gg/xQF9f9yUEM';
-      const InstagramLink = 'https://www.instagram.com/rtxxgg/';
+      const youtubeLink = 'https://discord.gg/s2bvv7tMaz';
+      const InstagramLink = 'https://www.youtube.com/@edwardo5422/videos';
       const { EmbedBuilder } = require('discord.js')
         const embed = new EmbedBuilder()
-            .setColor('#da2a41')
+            .setColor('#000000')
             .setAuthor({
           name: 'Owner',
-          iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          iconURL: 'https://cdn.discordapp.com/attachments/1048361437464121474/1255307766847766609/CuauFBt.png?ex=667d50b6&is=667bff36&hm=d6b34d859089cd900f404bee459b2c10c264b8e90e154eff602c57c393281a46&',
+          url: 'https://discord.gg/s2bvv7tMaz'
         })
-            .setDescription(`__**About me**__:\n\n ▶️ Myself Shiva aka GlaceYT. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n YouTube : ❤️ [RTX GAMING](${youtubeLink})\n Instagram : 💙 [rtxxgg](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n\n Edwardo\n YouTube : ❤️ [FurYx](${youtubeLink})\n Instagram : 💙 [rtxxgg](${InstagramLink})`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
