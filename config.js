bbe92c769867b314868ed0a530a22484244dadac
Zerokaloria5 <165279308+Zerokaloria5@users.noreply.github.com>
2024-06-26 16:56:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""],
+  ownerID: ["566725782634758152", ""],
   botInvite: "",
   supportServer: "",
   mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
